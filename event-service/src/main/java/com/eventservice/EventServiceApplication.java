f8f41fbe2c28cae2d3a5ca8cfa844725565761c6
Hikmet Kütük <hikmetkutuk@gmail.com>
2023-08-06 22:29:49 +0300

    created api gateway and service registery for microservices

--- event-service/src/main/java/com/eventservice/EventServiceApplication.java
+++ event-service/src/main/java/com/eventservice/EventServiceApplication.java
@@ -2,8 +2,10 @@ package com.eventservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EventServiceApplication {
 
     public static void main(String[] args) {
